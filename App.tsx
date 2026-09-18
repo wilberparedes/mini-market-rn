e92fd84401b16953849b76146fedd873f324f945
Wilber Paredes <wilberparedes@gmail.com>
2026-09-18 00:24:32 -0500

    feat: preload favorites

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Appearance } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 import { RootNavigator } from '@navigation/RootNavigator';
 import { queryClient } from './src/queryClient';
+import { useFavoriteStore } from '@store/favorites.store';
+
+Appearance.setColorScheme('light');
 
 const App = () => {
+  const loadFavorites = useFavoriteStore((state) => state.loadFavorites);
+
+  useEffect(() => {
+    loadFavorites();
+  }, [loadFavorites]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
