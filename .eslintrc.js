31ec30cb1c07ad391ca11c662e29884a9bf93a2e
Wilber Paredes <wilberparedes@gmail.com>
2026-09-17 23:23:33 -0500

    feat: config eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  ignorePatterns: ['node_modules/', 'android/', 'ios/', 'coverage/', 'build/'],
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  arrowParens: 'avoid',
   singleQuote: true,
   trailingComma: 'all',
+  semi: true,
+  printWidth: 80,
 };
