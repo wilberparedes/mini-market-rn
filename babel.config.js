74a67f11bb14ae2fe08d64cc0014833004d90b08
Wilber Paredes <wilberparedes@gmail.com>
2026-09-17 23:24:01 -0500

    feat: config babel jest and add dependences

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,42 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: path.resolve(__dirname, '.env'),
+});
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@api': './src/api',
+          '@domain': './src/domain',
+          '@config': './src/config',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@hooks': './src/hooks',
+          '@components': './src/components',
+          '@store': './src/store',
+          '@storage': './src/storage',
+          '@utils': './src/utils',
+          '@native': './src/native',
+        },
+      },
+    ],
+
+    [
+      'transform-inline-environment-variables',
+      {
+        include: ['API_URL'],
+      },
+    ],
+
+    'react-native-reanimated/plugin',
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,34 @@
 module.exports = {
   preset: 'react-native',
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(' +
+      [
+        '@react-native',
+        '@react-navigation',
+        '@tanstack',
+        'react-native',
+        'react-native-mmkv',
+        'react-native-reanimated',
+        'react-native-safe-area-context',
+        'react-native-screens',
+      ].join('|') +
+      ')/)',
+  ],
+
+  moduleNameMapper: {
+    '^react-native-mmkv$': '<rootDir>/__mocks__/react-native-mmkv.ts',
+
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@navigation/(.*)$': '<rootDir>/src/navigation/$1',
+    '^@screens/(.*)$': '<rootDir>/src/screens/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@storage/(.*)$': '<rootDir>/src/storage/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@native/(.*)$': '<rootDir>/src/native/$1',
+  },
 };
